3128d45c83b1c448541eec803744eb2413bc8e15
nduchenk <adddyny@yandex.com>
2024-10-16 17:30:59 +0200

    load data from rest endpoint

--- script.js
+++ script.js
@@ -11,17 +11,14 @@
 //     };
 //   }
 // );
-
-function updateDescriptionPage(id) {
-  const characterDescription = document.getElementById("characterDescription");
-  characterDescription.innerText = "Placeholder for ID: " + id;
-}
+const REST_DATA_SOURCE = "https://rickandmortyapi.com/api/character";
 
 // Set up buttons for each character to its description.
 let buttons = Array.from(document.getElementsByClassName("ButtonName"));
 
 for (let i = 0; i != buttons.length; ++i) {
   let button = buttons[i];
+
   button.addEventListener("click", function () {
     const description = document.getElementById(
       "characterDescriptionContainer"
@@ -30,8 +27,8 @@ for (let i = 0; i != buttons.length; ++i) {
       "charactersTableContainer"
     );
 
-    // update description page
-    updateDescriptionPage(button.id);
+    let characterId = this.getAttribute("data-characterId");
+    updateDescriptionPage(characterId);
     charactersTableContainer.style.display = "none";
     description.style.display = "block";
   });
@@ -50,3 +47,97 @@ backToCharactersTableButton.addEventListener("click", function () {
   description.style.display = "none";
   charactersTableContainer.style.display = "block";
 });
+
+async function updateTables() {
+  const jsonData = await getJsonData(REST_DATA_SOURCE);
+  const results = jsonData["results"];
+
+  if (results === null || !(results instanceof Array)) {
+    console.error("Received wrong data type: " + typeof results);
+    return;
+  }
+
+  // Hmmmm... Seems the order is always same and follows tree logic.
+  // Or it is better to select do select in a foor loop ???
+  let characterSpecies = Array.from(
+    document.getElementsByClassName("characterSpecies")
+  );
+  let characterStatus = Array.from(
+    document.getElementsByClassName("characterStatus")
+  );
+  let chracterButtons = Array.from(
+    document.getElementsByClassName("ButtonName")
+  );
+  let characterAvatars = Array.from(
+    document.getElementsByClassName("characterAvatar")
+  );
+
+  // TODO: refactor this spaghetti
+  for (let i = 0; i != results.length; ++i) {
+    let character = results[i];
+
+    let characterId = character["id"];
+    if (characterId === null || characterId === undefined) {
+      console.error("Character `id` is: ", typeof characterId);
+      continue;
+    }
+
+    let characterButton = chracterButtons[i];
+    characterButton.setAttribute("data-characterId", characterId);
+    let name = character["name"];
+    if (name === null || name === undefined) {
+      console.warn(`Name for id: ${characterId} is: ` + typeof name);
+    } else {
+      characterButton.innerText = name;
+    }
+
+    let status = character["status"];
+    if (status === null || status === undefined) {
+      console.warn(`Status for id: ${characterId} is: ` + typeof status);
+    } else {
+      characterStatus[i].innerText = status;
+    }
+
+    let species = character["species"];
+    if (species === null || species === undefined) {
+      console.warn(`Species for id: ${characterId} is: ` + typeof species);
+    } else {
+      characterSpecies[i].innerText = species;
+    }
+
+    let image_src = character["image"];
+    if (image_src === null || image_src === undefined) {
+      console.warn(
+        `Image source for id: ${characterId} is: ` + typeof image_src
+      );
+    } else {
+      characterAvatars[i].src = image_src;
+    }
+  }
+}
+
+// update description page
+function updateDescriptionPage(characterId) {
+  const characterDescription = document.getElementById("characterDescription");
+  characterDescription.innerText = "Placeholder for ID: " + characterId;
+}
+
+async function getJsonData(url) {
+  try {
+    const response = await fetch(url);
+
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+
+    const json = await response.json();
+    return json;
+  } catch (error) {
+    console.error(error.message);
+    return null;
+  }
+}
+
+(async () => {
+  await updateTables();
+})();
