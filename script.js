1d0fff4edad1f160b98d92d350b2115eab560b0d
nduchenk <31582256+nduchenk@users.noreply.github.com>
2024-10-18 13:47:15 +0200

    move Character into a spearate class and file (#11)
    * move Character class into a separate file
    
    * set uniq Id for each element for character class
    
    * rename setBeackButton to setBackButtonOnDescriptionView
    
    * add setters to Character class

--- script.js
+++ script.js
@@ -10,86 +10,8 @@ const STATIC_DATA_LENGTH = 20;
 //     };
 //   }
 // );
-const CHARACTERS_TABLE = new Array(STATIC_DATA_LENGTH);
+const CHARACTERS_TABLE = new Array();
 const REST_DATA_SOURCE = "https://rickandmortyapi.com/api/character";
-
-function Character(characterId, name, species, image_src, image_alt, status) {
-  this.characterId = characterId || null;
-  this.name = name || "";
-  this.species = species || "";
-  this.image_src = image_src || "";
-  this.image_alt = image_alt || "Placeholder for character Picture";
-  this.status = status || "";
-
-  this.createCharacterNode = function () {
-    let self = this;
-    // cell
-    const characterCell = document.createElement("td");
-    characterCell.classList.add("characterCell");
-    // cell-divcharacterCellContainer
-    const characterCellContainer = document.createElement("div");
-    characterCellContainer.classList.add("characterCellContainer");
-    characterCellContainer.setAttribute("data-characterId", self.characterId);
-    // image-div
-    const characterImageContainer = document.createElement("div");
-    characterImageContainer.classList.add("characterImageContainer");
-    // image
-    const image = document.createElement("img");
-    image.src = self.image_src;
-    image.alt = self.image_alt;
-    image.classList.add("characterAvatar");
-    // button
-    const button = document.createElement("button");
-    button.textContent = self.name;
-    button.classList.add("characterButton");
-    button.setAttribute("data-characterId", self.characterId);
-    button.addEventListener("click", function () {
-      // get characterId
-      let characterId = this.getAttribute("data-characterId");
-
-      const numCharacterId = Number(characterId);
-      if (!Number.isInteger(numCharacterId)) {
-        return;
-      }
-
-      getJsonData(REST_DATA_SOURCE + `/${characterId}`).then((jsonData) => {
-        // set new character description
-        const characterDescriptionContainer = document.getElementById(
-          "characterDescriptionContainer"
-        );
-        const characterDescription = document.getElementById(
-          "characterDescription"
-        );
-        characterDescription.innerText = JSON.stringify(jsonData);
-        const charactersTableContainer = document.getElementById(
-          "charactersTableContainer"
-        );
-
-        // change visibility of div elements
-        charactersTableContainer.style.display = "none";
-        characterDescriptionContainer.style.display = "block";
-      });
-    });
-
-    // species
-    const species = document.createElement("p");
-    species.textContent = self.species;
-    species.classList.add("characterSpecies");
-    // status
-    const status = document.createElement("p");
-    status.textContent = self.status;
-    status.classList.add("characterStatus");
-
-    characterImageContainer.appendChild(image);
-    characterCellContainer.appendChild(characterImageContainer);
-    characterCellContainer.appendChild(button);
-    characterCellContainer.appendChild(status);
-    characterCellContainer.appendChild(species);
-    characterCell.appendChild(characterCellContainer);
-
-    return characterCell;
-  };
-}
 
 function setInitialTablePattern() {
   function createTable() {
@@ -124,7 +46,7 @@ function setInitialTablePattern() {
   tableContainer.appendChild(table);
 }
 
-function setBackButton() {
+function setBackButtonOnDescriptionView() {
   // Set up back to table button.
   let backToCharactersTableButton = document.getElementById(
     "backToCharactersTableButton"
@@ -140,6 +62,37 @@ function setBackButton() {
     characterDescriptionContainer.style.display = "none";
     charactersTableContainer.style.display = "block";
   });
+}
+
+async function updateTables() {
+  const jsonData = await getJsonData(REST_DATA_SOURCE);
+  const results = jsonData["results"];
+  // const results = STATIC_DATA_SOURCE;
+
+  if (results === null || !(results instanceof Array)) {
+    console.error("Received wrong data type: " + typeof results);
+    return;
+  }
+
+  for (let i = 0; i != results.length; ++i) {
+    let character = results[i];
+    let cell = CHARACTERS_TABLE[i];
+
+    let characterId = character.id;
+    if (characterId === null || characterId === undefined) {
+      console.error("Character `id` is: ", typeof characterId);
+      continue;
+    }
+
+    // TODO: this might get wrong very easy, create spearate class for name, species, status, image and add type checks ?
+    cell.setCharacterProfile(
+      characterId,
+      character.name,
+      character.species,
+      character.status,
+      character.image
+    );
+  }
 }
 
 async function getJsonData(url) {
@@ -158,79 +111,9 @@ async function getJsonData(url) {
   }
 }
 
-async function updateTables() {
-  const jsonData = await getJsonData(REST_DATA_SOURCE);
-  const results = jsonData["results"];
-  // const results = STATIC_DATA_SOURCE;
-
-  if (results === null || !(results instanceof Array)) {
-    console.error("Received wrong data type: " + typeof results);
-    return;
-  }
-
-  // Hmmmm... Seems the order is always same and follows tree logic.
-  // Or it is better to select do select in a foor loop ???
-  let characterSpecies = Array.from(
-    document.getElementsByClassName("characterSpecies")
-  );
-  let characterStatus = Array.from(
-    document.getElementsByClassName("characterStatus")
-  );
-  let chracterButtons = Array.from(
-    document.getElementsByClassName("characterButton")
-  );
-  let characterAvatars = Array.from(
-    document.getElementsByClassName("characterAvatar")
-  );
-
-  // TODO: refactor this spaghetti
-  for (let i = 0; i != results.length; ++i) {
-    let character = results[i];
-
-    let characterId = character["id"];
-    if (characterId === null || characterId === undefined) {
-      console.error("Character `id` is: ", typeof characterId);
-      continue;
-    }
-
-    let characterButton = chracterButtons[i];
-    characterButton.setAttribute("data-characterId", characterId);
-    let name = character["name"];
-    if (name === null || name === undefined) {
-      console.warn(`Name for id: ${characterId} is: ` + typeof name);
-    } else {
-      characterButton.innerText = name;
-    }
-
-    let status = character["status"];
-    if (status === null || status === undefined) {
-      console.warn(`Status for id: ${characterId} is: ` + typeof status);
-    } else {
-      characterStatus[i].innerText = status;
-    }
-
-    let species = character["species"];
-    if (species === null || species === undefined) {
-      console.warn(`Species for id: ${characterId} is: ` + typeof species);
-    } else {
-      characterSpecies[i].innerText = species;
-    }
-
-    let image_src = character["image"];
-    if (image_src === null || image_src === undefined) {
-      console.warn(
-        `Image source for id: ${characterId} is: ` + typeof image_src
-      );
-    } else {
-      characterAvatars[i].src = image_src;
-      characterAvatars[i].alt = "Profile picture of " + name;
-    }
-  }
-}
-
 // kind of main function ;)
 (async () => {
-  setBackButton();
+  setBackButtonOnDescriptionView();
   setInitialTablePattern();
   await updateTables();
 })();
